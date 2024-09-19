7df144aa31540555de79d8f960bd9b4e268f95c9
Kuan Liao <61463206+Kuanliao1016@users.noreply.github.com>
2024-09-19 23:57:28 +0800

    updates

--- internal_validation.R
+++ internal_validation.R
@@ -42,7 +42,7 @@ for(i in 1:m ){
                                        resampled_data, 
                                        reverse = TRUE,
                                        timewt = "n/G2")$concordance
-    
+    brier(boot_md0, times = 365.25, newdata = resampled_data)
     #performance measures in the original data
     lp_orig<- predict(boot_md0, type = 'lp',newdata = impds)
     pred_prob_vl <- predict(boot_md0, type = "risk", time = 365.25, newdata = impds)
@@ -55,6 +55,7 @@ for(i in 1:m ){
                                        impds, 
                                        reverse = TRUE,
                                        timewt = "n/G2")$concordance
+    brier(boot_md0, times = 365.25, newdata = impds)
     #optimism
     result[j,1] <- AUC_boot$auc - AUC_orig$auc
     result[j,2] <-  harrell_C_boot - harrell_C_orig
@@ -1041,8 +1042,239 @@ summary(slope_res_app_symc,digits= 2)#time range weak calibration - calibration 
 summary(slope_fixed_res_app_symc,digits = 2)#fixed time point weak calibration - calibration slope
 
 
+summary(survfit(app_md,newdata = var_select_ds), times = 365.25)
+riskRegression::predictRisk(app_md,newdata = var_select_ds, times = 365.25)
 
 
+######################
+#Overall performance #
+######################
+#Bootstrap Resampling
+time1 <- Sys.time()
+B <- 500
+m <- 40
+result <- matrix(nrow = B, ncol = 8) 
+result_scaled <- matrix(nrow = B, ncol = 8) 
+result_change <- data.frame()
+result_change_scaled <- data.frame()
+result_bs_app <- data.frame()
+result_sbs_app <- data.frame()
+summary(result_bs_app)
+result_bs_corrected <- data.frame() 
+result_sbs_corrected <- data.frame() 
+result_change_bs_corrected <- data.frame() 
+result_change_sbs_corrected <- data.frame() 
+result_app <- data.frame() 
+set.seed(1429)
+for(i in 1:m) {
+  impds <- implong_pts_predict %>% filter(.imp == m)
+  climatological_fit <- survfit(Surv(surv_time, event) ~ 1, data = impds)
+  climatological_bs <- mean(summary(climatological_fit, times = 365.25)$surv^2)
+  for (j in 1:B) {
+    samp_index <- sample(1:nrow(impds), nrow(impds), rep = TRUE)
+    samp_index[order(samp_index)]
+    
+    # Bootstrapping data
+    resampled_data <- impds[samp_index, ]
+    
+    # Boot model
+    boot_md0 <- coxph(Surv(surv_time, event) ~ histology_coded + ps + NLR + age, data = resampled_data)
+    boot_md0_eq5d <- update(boot_md0, . ~ . + eq_5d)
+    boot_md0_symb <- update(boot_md0, . ~ . + symburden)
+    boot_md0_symc <- update(boot_md0, . ~ . + symcount)
+    
+    # Brier scores for resampled data
+    bs_resample_0 <- brier(boot_md0, times = 365.25, newdata = resampled_data)
+    bs_resample_eq5d <- brier(boot_md0_eq5d, times = 365.25, newdata = resampled_data)
+    bs_resample_symb <- brier(boot_md0_symb, times = 365.25, newdata = resampled_data)
+    bs_resample_symc <- brier(boot_md0_symc, times = 365.25, newdata = resampled_data)
+    
+    # Brier scores for original data
+    bs_new <- brier(boot_md0, times = 365.25, newdata = impds)
+    bs_new_eq5d <- brier(boot_md0_eq5d, times = 365.25, newdata = impds)
+    bs_new_symb <- brier(boot_md0_symb, times = 365.25, newdata = impds)
+    bs_new_symc <- brier(boot_md0_symc, times = 365.25, newdata = impds)
+    
+    #change in brier
+    change_resample_eq5d <- bs_resample_eq5d$brier - bs_resample_0$brier
+    change_resample_symb <- bs_resample_symb$brier - bs_resample_0$brier
+    change_resample_symc <- bs_resample_symc$brier - bs_resample_0$brier
+    
+    
+    # Change in Brier scores for original data
+    change_new_eq5d <- bs_new_eq5d$brier - bs_new$brier
+    change_new_symb <- bs_new_symb$brier - bs_new$brier
+    change_new_symc <- bs_new_symc$brier - bs_new$brier
+    
+    # Optimism
+    result[j, 1] <- bs_resample_0$brier - bs_new$brier
+    result[j, 2] <- bs_resample_eq5d$brier - bs_new_eq5d$brier 
+    result[j, 3] <- bs_resample_symb$brier -  bs_new_symb$brier
+    result[j, 4] <- bs_resample_symc$brier - bs_new_symc$brier
+    result[j, 5] <- bs_resample_0$brier
+    result[j, 6] <- bs_resample_eq5d$brier
+    result[j, 7] <- bs_resample_symb$brier
+    result[j, 8] <- bs_resample_symc$brier
+    
+    # Store optimism for change in BS
+    result_change[j, 1] <- change_new_eq5d - change_resample_eq5d
+    result_change[j, 2] <- change_new_symb - change_resample_symb
+    result_change[j, 3] <- change_new_symc - change_resample_symc
+    # Calculate BSmax
+    climatological_fit_resampled <- survfit(Surv(surv_time, event) ~ 1, data = resampled_data)
+    climatological_bs_resampled <- mean(summary(climatological_fit, times = 365.25)$surv^2)
+    
+    # Calculate scaled Brier scores in the validation set
+    scaled_bs_new <- 1- (bs_new$brier / climatological_bs) 
+    scaled_bs_new_eq5d <- 1- (bs_new_eq5d$brier / climatological_bs) 
+    scaled_bs_new_symb <- 1-(bs_new_symb$brier / climatological_bs) 
+    scaled_bs_new_symc <- 1-(bs_new_symc$brier / climatological_bs) 
+    # Calculate scaled Brier scores in the development set    
+    scaled_bs_resample_0 <- 1-(bs_resample_0$brier / climatological_bs_resampled) 
+    scaled_bs_resample_eq5d <- 1- (bs_resample_eq5d$brier / climatological_bs_resampled)
+    scaled_bs_resample_symb <- 1-(bs_resample_symb$brier / climatological_bs_resampled) 
+    scaled_bs_resample_symc <- 1-(bs_resample_symc$brier / climatological_bs_resampled)
+    
+    # Optimism for scaled Brier scores
+    result_scaled[j, 1] <- scaled_bs_resample_0 - scaled_bs_new 
+    result_scaled[j, 2] <- scaled_bs_resample_eq5d - scaled_bs_new_eq5d
+    result_scaled[j, 3] <- scaled_bs_resample_symb - scaled_bs_new_symb
+    result_scaled[j, 4] <- scaled_bs_resample_symc - scaled_bs_new_symc
+    result_scaled[j, 5] <- scaled_bs_resample_0 
+    result_scaled[j, 6] <- scaled_bs_resample_eq5d 
+    result_scaled[j, 7] <- scaled_bs_resample_symb
+    result_scaled[j, 8] <- scaled_bs_resample_symc
+    # Change in scaled Brier scores for original data
+    change_new_scaled_eq5d <- scaled_bs_new_eq5d - scaled_bs_new
+    change_new_scaled_symb <- scaled_bs_new_symb - scaled_bs_new
+    change_new_scaled_symc <- scaled_bs_new_symc - scaled_bs_new
+    
+    # Change in scaled Brier scores for resampled data
+    change_resample_scaled_eq5d <- scaled_bs_resample_eq5d - scaled_bs_resample_0
+    change_resample_scaled_symb <- scaled_bs_resample_symb - scaled_bs_resample_0
+    change_resample_scaled_symc <- scaled_bs_resample_symc - scaled_bs_resample_0
+    
+    # Optimism for change in scaled Brier scores
+    result_change_scaled[j, 1] <- change_new_scaled_eq5d - change_resample_scaled_eq5d
+    result_change_scaled[j, 2] <- change_new_scaled_symb - change_resample_scaled_symb
+    result_change_scaled[j, 3]  <- change_new_scaled_symc - change_resample_scaled_symc
+  }
+  
+  boot_md1 <- coxph(Surv(surv_time, event) ~ histology_coded + ps + NLR + firstline + age, data = impds)
+  boot_md1_eq5d <- update(boot_md1, . ~ . + eq_5d)
+  boot_md1_symb <- update(boot_md1, . ~ . + symburden)
+  boot_md1_symc <- update(boot_md1, . ~ . + symcount)
 
+  
+  bs_app <- brier(boot_md1, times = 365.25, newdata = impds)
+  bs_app_eq5d <- brier(boot_md1_eq5d, times = 365.25, newdata = impds)
+  bs_app_symb <- brier(boot_md1_symb, times = 365.25, newdata = impds)
+  bs_app_symc <- brier(boot_md1_symc, times = 365.25, newdata = impds)
+  
+  
+  # Calculate apparent scaled Brier scores 
+  scaled_bs_app <- 1- (bs_app$brier / climatological_bs) 
+  scaled_bs_app_eq5d <- 1- (bs_app_eq5d$brier / climatological_bs) 
+  scaled_bs_app_symb <- 1- (bs_app_symb$brier / climatological_bs)
+  scaled_bs_app_symc <- 1- (bs_app_symc$brier / climatological_bs)
+  
+  #Apparent bs
+  result_bs_app[i,1] <- bs_app$brier
+  result_bs_app[i,2:3] <- quantile(result[, 5], c(0.025, 0.975))
+  result_bs_app[i,4] <- bs_app_eq5d$brier
+  result_bs_app[i,5:6] <- quantile(result[, 6], c(0.025, 0.975))
+  result_bs_app[i,7] <- bs_app_symb$brier
+  result_bs_app[i,8:9] <- quantile(result[, 7], c(0.025, 0.975))
+  result_bs_app[i,10] <- bs_app_symc$brier
+  result_bs_app[i,11:12] <- quantile(result[, 8], c(0.025, 0.975))  
+  
+  #Apparent sbs
+  result_sbs_app[i,1] <- scaled_bs_app
+  result_sbs_app[i,2:3] <- quantile(result_scaled[, 5], c(0.025, 0.975))
+  result_sbs_app[i,4] <- scaled_bs_app_eq5d
+  result_sbs_app[i,5:6] <- quantile(result_scaled[, 6], c(0.025, 0.975))
+  result_sbs_app[i,7] <- scaled_bs_app_symb
+  result_sbs_app[i,8:9] <- quantile(result_scaled[, 7], c(0.025, 0.975))
+  result_sbs_app[i,10] <- scaled_bs_app_symb
+  result_sbs_app[i,11:12] <- quantile(result_scaled[, 8], c(0.025, 0.975)) 
+
+  # Optimism corrected bs
+  result_bs_corrected[i, 1:3] <- bs_app$brier - quantile(result[, 1], c(0.025, 0.5, 0.975))
+  result_bs_corrected[i, 4:6] <- bs_app_eq5d$brier - quantile(result[, 2], c(0.025, 0.5, 0.975))
+  result_bs_corrected[i, 7:9] <- bs_app_symb$brier - quantile(result[, 3], c(0.025, 0.5, 0.975))
+  result_bs_corrected[i, 10:12] <- bs_app_symc$brier - quantile(result[, 4], c(0.025, 0.5, 0.975))
+  
+  # Optimism corrected sbs
+  result_sbs_corrected[i, 1:3] <- scaled_bs_app - quantile(result_scaled[, 1], c(0.025, 0.5, 0.975))
+  result_sbs_corrected[i, 4:6] <- scaled_bs_app_eq5d - quantile(result_scaled[, 2], c(0.025, 0.5, 0.975))
+  result_sbs_corrected[i, 7:9] <- scaled_bs_app_symb - quantile(result_scaled[, 3], c(0.025, 0.5, 0.975))
+  result_sbs_corrected[i, 10:12] <- scaled_bs_app_symc - quantile(result_scaled[, 4], c(0.025, 0.5, 0.975))
+  
+
+  # Calculate apparent change in Brier scores
+  change_bs_app_eq5d <- bs_app_eq5d$brier - bs_app$brier
+  change_bs_app_symb <- bs_app_symb$brier - bs_app$brier
+  change_bs_app_symc <- bs_app_symc$brier - bs_app$brier
+  
+  # Optimism-corrected change in Brier scores
+  result_change_bs_corrected[i, 1:3] <- change_bs_app_eq5d - quantile(result_change[, 1], c(0.025, 0.5, 0.975))
+  result_change_bs_corrected[i, 4:6] <- change_bs_app_symb - quantile(result_change[, 2], c(0.025, 0.5, 0.975))
+  result_change_bs_corrected[i, 7:9] <- change_bs_app_symc - quantile(result_change[, 3], c(0.025, 0.5, 0.975))
+  
+  # Calculate the apparent change in scaled Brier scores
+  change_sbs_app_eq5d <- scaled_bs_app_eq5d - scaled_bs_app
+  change_sbs_app_symb <- scaled_bs_app_symb - scaled_bs_app
+  change_sbs_app_symc <- scaled_bs_app_symc - scaled_bs_app
+  
+  # Optimism-corrected change in scaled Brier scores
+  result_change_sbs_corrected[i, 1:3] <- change_sbs_app_eq5d - quantile(result_change_scaled[, 1], c(0.025, 0.5, 0.975))
+  result_change_sbs_corrected[i, 4:6] <- change_sbs_app_symb - quantile(result_change_scaled[, 2], c(0.025, 0.5, 0.975))
+  result_change_sbs_corrected[i, 7:9] <- change_sbs_app_symc - quantile(result_change_scaled[, 3], c(0.025, 0.5, 0.975))
+  
+}
+time2 <- Sys.time()
+time2-time1
+colnames(result_bs_app) <- c(
+  "bs_app_mean", "bs_app_95CI_lower", "bs_app_95CI_upper", 
+  "bs_app_eq5d_mean", "bs_app_eq5d_95CI_lower", "bs_app_eq5d_95CI_upper", 
+  "bs_app_symb_mean", "bs_app_symb_95CI_lower", "bs_app_symb_95CI_upper", 
+  "bs_app_symc_mean", "bs_app_95CI_lower", "bs_app_95CI_upper"
+)
+
+summary(result_bs_app)
+colnames(result_sbs_app) <- c(
+  "scaled_bs_app_mean", "scaled_bs_app_95CI_lower", "scaled_bs_app_95CI_upper", 
+  "scaled_bs_app_eq5d_mean", "scaled_bs_app_eq5d_95CI_lower", "scaled_bs_app_eq5d_95CI_upper", 
+  "scaled_bs_app_symb_mean", "scaled_bs_app_symb_95CI_lower", "scaled_bs_app_symb_95CI_upper", 
+  "scaled_bs_app_symc_mean", "scaled_bs_app_symc_95CI_lower", "scaled_bs_app_symc_95CI_upper"
+)
+summary(result_sbs_app)
+
+colnames(result_bs_corrected) <- c(
+  "BS_Corrected_Lower", "BS_Corrected_Median", "BS_Corrected_Upper",
+  "BS_Corrected_EQ5D_Lower", "BS_Corrected_EQ5D_Median", "BS_Corrected_EQ5D_Upper",
+  "BS_Corrected_Symb_Lower", "BS_Corrected_Symb_Median", "BS_Corrected_Symb_Upper",
+  "BS_Corrected_Symc_Lower", "BS_Corrected_Symc_Median", "BS_Corrected_Symc_Upper"
+)
+summary(result_bs_corrected)
+colnames(result_sbs_corrected) <- c(
+  "SBS_Corrected_Lower", "SBS_Corrected_Median", "SBS_Corrected_Upper",
+  "SBS_Corrected_EQ5D_Lower", "SBS_Corrected_EQ5D_Median", "SBS_Corrected_EQ5D_Upper",
+  "SBS_Corrected_Symb_Lower", "SBS_Corrected_Symb_Median", "SBS_Corrected_Symb_Upper",
+  "SBS_Corrected_Symc_Lower", "SBS_Corrected_Symc_Median", "SBS_Corrected_Symc_Upper"
+)
+summary(result_sbs_corrected)
+colnames(result_change_bs_corrected) <- c(
+  "Change_BS_Corrected_EQ5D_Lower", "Change_BS_Corrected_EQ5D_Median", "Change_BS_Corrected_EQ5D_Upper",
+  "Change_BS_Corrected_Symb_Lower", "Change_BS_Corrected_Symb_Median", "Change_BS_Corrected_Symb_Upper",
+  "Change_BS_Corrected_Symc_Lower", "Change_BS_Corrected_Symc_Median", "Change_BS_Corrected_Symc_Upper"
+)
+summary(result_change_bs_corrected,digits = 2)
+colnames(result_change_sbs_corrected) <- c(
+  "Change_SBS_Corrected_EQ5D_Lower", "Change_SBS_Corrected_EQ5D_Median", "Change_SBS_Corrected_EQ5D_Upper",
+  "Change_SBS_Corrected_Symb_Lower", "Change_SBS_Corrected_Symb_Median", "Change_SBS_Corrected_Symb_Upper",
+  "Change_SBS_Corrected_Symc_Lower", "Change_SBS_Corrected_Symc_Median", "Change_SBS_Corrected_Symc_Upper"
+)
+summary(result_change_bs_corrected,digits = 2)
 
 
